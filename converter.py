7fd8218776e21543039a2d52a6bc15aa7afe6e02
hcxie20 <hcxie20@gmail.com>
2021-07-20 23:35:35 +0800

    done function. todo: readme and dependencied

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 import os
-# from pdf2image import convert_from_path
+from pdf2image import convert_from_path
  
  
 # Store Pdf with convert_from_path function
@@ -7,15 +7,15 @@ import os
 def is_pdf(file_name, file_path):
     return os.path.isfile(os.path.join(file_path, file_name)) and file_name[-4:] == ".pdf"
 
-pdf_dir = "c:\\Users\haochenxie\Desktop\docs\第二医院"
+pdf_dir = "/Users/xiehaochen/Desktop/hospital/第二医院"
 pdf_names = [file_name for file_name in os.listdir(pdf_dir) if is_pdf(file_name, pdf_dir)]
 
 def convert_to_image(file_name, file_path):
     images = convert_from_path(os.path.join(file_path, file_name))
 
     for i in range(len(images)):
-        print(os.path.join(file_path, file_name[-4:]) + '_page_' + ('{0}'.format()).zfill(len(str(len(images)))))
-        # images[i].save(os.path.join(file_path, file_name[-4:]) + '_page_' + ('{0}'.format()).zfill(len(str(len(images)))))
+        print(os.path.join(file_path, file_name[:-4]) + '_page_' + ('{0}'.format(i + 1)).zfill(len(str(len(images)))) + '.jpg')
+        images[i].save(os.path.join(file_path, file_name[:-4]) + '_page_' + ('{0}'.format(i + 1)).zfill(len(str(len(images)))) + '.jpg')
 
 for file_name in pdf_names:
     convert_to_image(file_name, pdf_dir)
